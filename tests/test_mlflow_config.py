4f66ec7c0fca3ce6898c6e17757676dc74b05cf4
Ishan Thakkar <202301245@dau.ac.in>
2026-07-16 16:45:51 +0530

    refactor: streamline dashboard navigation, remove obsolete model modules, and update project documentation

--- tests/test_mlflow_config.py
+++
@@ -1,9 +0,0 @@
-from src.csr.config.configuration import ConfigurationManager
-from src.csr.constants import MLFLOW_EXPERIMENT_NAME, MLFLOW_TRACKING_URI
-
-
-def test_mlflow_config_uses_environment() -> None:
-    config = ConfigurationManager().get_mlflow_config()
-
-    assert config.tracking_uri == MLFLOW_TRACKING_URI
-    assert config.experiment_name == MLFLOW_EXPERIMENT_NAME
